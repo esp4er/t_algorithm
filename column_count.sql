b86ae900d252b71742f48a12fe713706937ff77b
esp4er <esp4er@users.noreply.github.com>
2015-12-22 19:02:06 +0100

    Update column_count.sql

--- column_count.sql
+++ column_count.sql
@@ -1,13 +1,24 @@
+drop table temp;
 SET SERVEROUTPUT ON;
 DECLARE
-  v_names VARCHAR2(10);
+  v_names  VARCHAR2(10);
   counting NUMBER NOT NULL :=1;
-  v_id NUMBER;
+  v_id     NUMBER;
 BEGIN
+  EXECUTE IMMEDIATE 'create table temp (        
+CZEST NUMBER,        
+MAXIMUM NUMBER      
+)';
   LOOP
     SELECT view_name INTO v_names FROM v_name WHERE id=counting;
-    SELECT max(id) into v_id from v_name; 
-    EXECUTE immediate 'SELECT COUNT(WYNIK) AS freq, WYNIK FROM '|| v_names|| ' GROUP BY WYNIK ORDER BY WYNIK';
+    SELECT MAX(id) INTO v_id FROM v_name;
+    EXECUTE IMMEDIATE 'INSERT INTO temp (czest, maximum)    
+SELECT MAX(      
+(SELECT MAX(COUNT(WYNIK)) AS freq FROM ' ||v_names|| ' GROUP BY WYNIK      
+)) AS czest,      
+COUNT((SELECT COUNT(wynik) FROM '||v_names|| '      
+)) AS maximum      
+FROM '||v_names||'';
     counting:=counting+1;
     EXIT
   WHEN counting=v_id+1;
@@ -16,4 +27,4 @@ END;
 /
 CREATE OR REPLACE VIEW v_name
 AS
-  SELECT view_name, rownum AS id FROM user_views WHERE view_name LIKE '%DATA%';
+  SELECT view_name, rownum AS id FROM user_views WHERE view_name LIKE '%DATA%'; 
