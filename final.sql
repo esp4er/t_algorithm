12ceb0f6a4127b30dfcbdb9b756925a48a1874af
esp4er <esp4er@users.noreply.github.com>
2015-12-22 20:50:29 +0100

    Create final.sql

---
+++ final.sql
@@ -0,0 +1,19 @@
+DECLARE
+  view_name VARCHAR(20);
+BEGIN
+  LOOP
+  EXIT WHEN
+  END LOOP;
+END;
+/
+
+select maximum-czest
+from v_temp 
+
+--CASE
+--    WHEN grade = 'A' THEN DBMS_OUTPUT.PUT_LINE('Excellent');
+--    WHEN grade = 'B' THEN DBMS_OUTPUT.PUT_LINE('Very Good');
+--    WHEN grade = 'C' THEN DBMS_OUTPUT.PUT_LINE('Good');
+--    WHEN grade = 'D' THEN DBMS_OUTPUT.PUT_LINE('Fair');
+--    WHEN grade = 'F' THEN DBMS_OUTPUT.PUT_LINE('Poor');
+--  END CASE;
