05f9b69c172c542277e040809fe43ee3cddb5258
esp4er <esp4er@users.noreply.github.com>
2015-12-28 19:00:57 +0100

    Update column_count.sql

--- column_count.sql
+++ column_count.sql
@@ -1,7 +1,7 @@
 --Tworzenie widoku z nazwami pozostaych widoków
 CREATE OR REPLACE VIEW v_name
 AS
-  SELECT view_name, rownum AS id FROM user_views WHERE view_name LIKE '%DATA%';
+SELECT view_name, rownum AS id FROM user_views WHERE view_name LIKE '%DATA%';
   DROP TABLE temp;
   DROP TRIGGER TEMP_ID;
   DROP SEQUENCE temp_seq;
@@ -13,21 +13,24 @@ AS
       VIEW_NAME VARCHAR2(50),
       CONSTRAINT "TEMP_ID_PK" PRIMARY KEY ("ID")
     );
+CREATE SEQUENCE "T_ALGORITHM"."TEMP_SEQ" MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER NOCYCLE ;    
 CREATE OR REPLACE TRIGGER "T_ALGORITHM"."TEMP_ID" BEFORE
   INSERT ON temp FOR EACH ROW BEGIN :new.id := temp_seq.nextval;
 END;
 /
 ALTER TRIGGER "T_ALGORITHM"."TEMP_ID" ENABLE;
-CREATE SEQUENCE "T_ALGORITHM"."TEMP_SEQ" MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER NOCYCLE ;
   SET SERVEROUTPUT ON;
   DECLARE
     v_names  VARCHAR2(10);
     counting NUMBER NOT NULL :=1;
     v_id     NUMBER;
+    row_num number;
+    row_count number:=1;
   BEGIN
     LOOP
       SELECT view_name INTO v_names FROM v_name WHERE id=counting;
       SELECT MAX(id) INTO v_id FROM v_name;
+      Select a.OBJECT_COUNT into row_num from pom a;
       EXECUTE IMMEDIATE 'INSERT INTO temp (czest, maximum)    
 SELECT MAX(      
 (SELECT MAX(COUNT(WYNIK)) AS freq FROM ' ||v_names|| ' GROUP BY WYNIK      
@@ -36,13 +39,15 @@ COUNT((SELECT COUNT(wynik) FROM '||v_names|| '
 )) AS maximum      
 FROM '||v_names||'';
       EXECUTE IMMEDIATE 'update temp set VIEW_NAME = (SELECT VIEW_NAME FROM v_name where id='||counting||')where id='||counting||'';
-      EXECUTE IMMEDIATE '';
-      --execute IMMEDIATE 'CREATE OR REPLACE VIEW v_temp
-      --AS
-      --  SELECT a.CZEST, a.ID, a.MAXIMUM, d.VIEW_NAME FROM temp a, v_name d WHERE a.id=d.id';
       counting:=counting+1;
       EXIT
     WHEN counting=v_id+1;
     END LOOP;
+          loop
+execute immediate 'create or replace view v_wynik'||row_count||' as
+select a.view_name, (a.MAXIMUM-a.CZEST) as wynik from temp a where view_name like ''V_DATA'||row_count||'%'' order by a.VIEW_NAME ';
+row_count:=row_count+1;
+exit when row_count=row_num+1;
+end loop;
   END;
   /
